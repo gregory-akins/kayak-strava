e4ebc96372b4f78383b4c1c0359557ccddae9249
Greg Akins <gregory.akins@icf.com>
2023-12-28 08:50:03 -0500

    Added in 'Import' check and integrated totalActivities count for paging

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,40 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import axios from "axios";
 import Activity from "./models/Activity";
-import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
-import { Container, Button } from "@mui/material";
+import {
+  DataGrid,
+  GridRowsProp,
+  GridColDef,
+  GridRenderCellParams,
+} from "@mui/x-data-grid";
+import { Container, Button, Checkbox } from "@mui/material";
 
 import { useServiceConfig } from "@akinsgre/kayak-strava-utility";
 import StravaRedirect from "./StravaRedirect";
+function RenderImport(props: GridRenderCellParams<any, any>) {
+  const [checked, setChecked] = useState(false); // Initiated react binded value with param from `rows`
 
-const columns = [
+  // Handler for user clicks to set checkbox mark or unset it
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    setChecked(event.target.checked);
+    // We'll need to get the row ID.. this should probably be something on the checkbox
+  };
+  //The bind for dynamic mark/unmark: checked={checked}
+  //The handler for user clicks: onChange={handleChange}
+  return <Checkbox id="1" checked={checked} onChange={handleChange} />;
+}
+const columns: GridColDef[] = [
+  {
+    field: "imported",
+    headerName: "Import?",
+    width: 150,
+    renderCell: RenderImport,
+  },
   {
     field: "id",
     headerName: "ID",
-    width: 70,
+    width: 140,
   },
   {
     field: "date",
@@ -29,6 +52,7 @@ export default function App() {
   const label = { inputProps: { "aria-label": "Checkbox demo" } };
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [activities, setActivities] = useState<Array<Activity>>([]);
+
   const [page, setPage] = useState(1);
   const [needActivities, setNeedActivities] = useState<Boolean>(true);
   const [pageState, setPageState] = useState({
@@ -39,29 +63,50 @@ export default function App() {
     pageSize: 10,
   });
 
+  const [totalActivities, setTotalActivities] = useState();
+
+  const getTotalActivities = () => {
+    let accessToken: String = sessionStorage.getItem("accessToken");
+    //ToDO let's fix the useServiceConfig to use a different name
+    /*eslint-disable */
+    useServiceConfig().then((value) => {
+      if (accessToken) {
+        const callActivities: string = `${value.kayakStravaUrl}/athlete/totalactivities?&accessToken=${accessToken}`;
+        axios
+          .get(callActivities)
+          .then((res) => res.data)
+          .then((data) => {
+            setTotalActivities(data);
+          })
+          .catch((error) => {
+            if (error?.response?.status === 401) {
+            }
+          });
+      }
+    });
+
+    /*eslint-enable */
+  };
   const getActivities = async (page: number = 1) => {
     let accessToken: String = sessionStorage.getItem("accessToken");
     //ToDO let's fix the useServiceConfig to use a different name
     /*eslint-disable */
     useServiceConfig().then((value) => {
       if (accessToken) {
-        const callActivities: string = `${value.kayakStravaUrl}/athlete/activities?page=${pageState.page}&accessToken=${accessToken}`;
+        const callActivities: string = `${value.kayakStravaUrl}/athlete/activities?page=${pageState.page}&accessToken=${accessToken}&per_page=${pageState.pageSize}`;
+
         axios
           .get(callActivities)
           .then((res) => res.data)
           .then((data) => {
-            console.log("Did we get data from server?", data);
             const kayakingData: Array<Activity> = [];
             data.forEach((element) => {
-              if (element.type === "Kayaking") {
-                const kayakElement: Activity = new Activity();
-                kayakElement.id = element.id;
-                kayakElement.type = element.type;
-                kayakElement.name = element.name;
-                kayakElement.date = element.start_date_local;
-
-                kayakingData.push(kayakElement);
-              }
+              const kayakElement: Activity = new Activity();
+              kayakElement.id = element.id;
+              kayakElement.type = element.type;
+              kayakElement.name = element.name;
+              kayakElement.date = element.start_date_local;
+              kayakingData.push(kayakElement);
             });
             setActivities(kayakingData);
             setNeedActivities(false);
@@ -78,24 +123,35 @@ export default function App() {
     });
     /*eslint-enable */
   };
-  // (guarantee useEffect deps are in sync with useWhatChanged)
-  //activities, needActivities, pageState.page pageState.pageSize,
-
-  useEffect(() => {
-    getActivities();
-  }, [pageState.page, pageState.pageSize]);
-
+  useMemo(() => getTotalActivities(), []);
+  //uwc-debug
   useEffect(() => {
     setPageState((old) => ({ ...old, isLoading: true }));
-
+    getActivities().then(() => {
+      pageState.data = activities;
+    });
     setPageState((old) => ({
       ...old,
       isLoading: false,
       data: activities,
-      total: 10,
     }));
   }, [pageState.page, pageState.pageSize]);
+  //uwc-debug
+  useEffect(() => {
+    setPageState((old) => ({
+      ...old,
+      isLoading: false,
+      data: activities,
+      total: totalActivities,
+    }));
+  }, [activities, pageState.total]);
 
+  useEffect(() => {
+    setPageState((old) => ({
+      ...old,
+      total: totalActivities ? totalActivities : 4,
+    }));
+  }, [totalActivities]);
   const rows: GridRowsProp = activities;
 
   return (
@@ -113,9 +169,7 @@ export default function App() {
         <Container style={{ marginTop: 100, marginBottom: 100 }}>
           <DataGrid
             autoHeight
-            rows={(() => {
-              return pageState.data;
-            })()}
+            rows={pageState.data}
             rowCount={pageState.total}
             loading={pageState.isLoading}
             rowsPerPageOptions={[10, 30, 50, 70, 100]}
